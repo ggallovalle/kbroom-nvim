82c1e6074d65e842103d60b963e52331f59b3f7d
Gerson Gallo <ggallovalle@gmail.com>
2024-10-24 02:36:45 -0600

    refactor(setup): move diagnostic to its own setup file

--- init.lua
+++ init.lua
@@ -21,27 +21,7 @@ require("setup.keymap").setup()
 require("setup.user_command").setup()
 require("setup.autocmd").setup()
 require("setup.netrw").setup()
-
-
--- {{{ diagnostic
--- Diagnostic appearance
-vim.diagnostic.config({
-  float = {
-    border = "rounded",
-    format = function(diagnostic)
-      -- "ERROR (line n): message"
-      return string.format("%s (line %i): %s",
-        vim.diagnostic.severity[diagnostic.severity],
-        diagnostic.lnum + 1,
-        diagnostic.message)
-    end
-  },
-  update_in_insert = false,
-})
-
--- Keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
--- }}}
+require("setup.diagnostic").setup()
 
 
 -- {{{ lazy.nvim
---
+++ lua/setup/diagnostic.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.setup()
+    -- {{{ diagnostic
+    -- Diagnostic appearance
+    vim.diagnostic.config({
+        float = {
+            border = "rounded",
+            format = function(diagnostic)
+                -- "ERROR (line n): message"
+                return string.format("%s (line %i): %s",
+                    vim.diagnostic.severity[diagnostic.severity],
+                    diagnostic.lnum + 1,
+                    diagnostic.message)
+            end
+        },
+        update_in_insert = false,
+    })
+
+    -- Keymaps
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+    -- }}}
+end
+
+return M
