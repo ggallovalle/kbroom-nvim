aa0c08c49a100685860e554719458a7e0acf265a
Gerson Gallo <ggallovalle@gmail.com>
2024-10-24 02:31:40 -0600

    refactor(setup): move user commands to its own setup file

--- init.lua
+++ init.lua
@@ -18,21 +18,7 @@ vim.g.have_nerd_font = true
 
 require("setup.options").setup()
 require("setup.keymap").setup()
-
-
--- {{{ commands
---- Trims trailing whitespace
---- \s: white space char, \+ :one or more, $: end of the line, e: suppresses warning when no match found, c: confirm
-local function trim_whitespace()
-  local win_save = vim.fn.winsaveview()
-  vim.cmd([[keeppatterns %s/\s\+$//ec]])
-  vim.fn.winrestview(win_save)
-end
-vim.api.nvim_create_user_command("TrimWhitespace", trim_whitespace, { nargs = 0 })
-
--- Changes current working directory
-vim.api.nvim_create_user_command("CD", ":lcd %:h", { nargs = 0 })
--- }}}
+require("setup.user_command").setup()
 
 
 -- {{{ autocmds
