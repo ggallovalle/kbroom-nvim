05d00a1cde9428e1bce861483b1e1f0873b9bbf1
Gerson Gallo <ggallovalle@gmail.com>
2024-10-25 23:53:43 -0600

    test: setup minit

---
+++ tests/minit.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S nvim -l
+
+vim.env.LAZY_STDPATH = ".tests"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy.minit").setup({
+  spec = {
+    { dir = vim.uv.cwd() },
+  },
+})
+
