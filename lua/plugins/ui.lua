38efa365d1920c2683035b5b36c19991cfd39659
Gerson Gallo <ggallovalle@gmail.com>
2025-04-01 23:30:20 -0600

    feat: make lualine more barebones

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,11 +1,35 @@
+local H = {}
+
+H.first_str = function (value)
+  return value:sub(1, 1)
+end
+
 local Lualine = {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require('lualine').setup()
-    end
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      sections = {
+        lualine_a = { { 'mode', fmt = H.first_str } },
+        lualine_b = { 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+
+    })
+  end
 }
 
 return {
-    Lualine
+  Lualine
 }
