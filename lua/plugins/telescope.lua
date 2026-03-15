d38f3cb14ce7456f444f2fbf2b21b2854c712571
Gerson Gallo <ggallovalle@gmail.com>
2026-03-15 08:13:09 -0600

    feat: telescope streamline extensions installed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,8 @@
---- plugins/telescope.lua
---- Telescope settings
----@require ripgrep: https://github.com/BurntSushi/ripgrep
----@require C compiler
----@require Make
-
----@type LazyPluginSpec[]
-local plugins = {}
-
----@type LazyPluginSpec
-local Plenary = {
-  url = "https://github.com/nvim-lua/plenary.nvim.git",
-  -- commit b9fd522 (date unknown, from lazy-lock) https://github.com/nvim-lua/plenary.nvim/commit/b9fd5226c2f76c951fc8ed5923d85e4de065e509
-  commit = "b9fd5226c2f76c951fc8ed5923d85e4de065e509",
-}
-
 ---@type LazyPluginSpec
 local TelescopeFzfNative = {
   url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim.git",
-  -- commit 6fea601 (date unknown, from lazy-lock) https://github.com/nvim-telescope/telescope-fzf-native.nvim/commit/6fea601bd2b694c6f2ae08a6c6fab14930c60e2c
+  -- last version check: 2026-03-15
+  -- commit date: 2025-11-07
   commit = "6fea601bd2b694c6f2ae08a6c6fab14930c60e2c",
   build = "make",
   cond = function()
@@ -26,33 +11,16 @@ local TelescopeFzfNative = {
 }
 
 ---@type LazyPluginSpec
-local TelescopeUiSelect = {
-  url = "https://github.com/nvim-telescope/telescope-ui-select.nvim.git",
-  -- commit 6e51d7d (date unknown, from lazy-lock) https://github.com/nvim-telescope/telescope-ui-select.nvim/commit/6e51d7da30bd139a6950adf2a47fda6df9fa06d2
-  commit = "6e51d7da30bd139a6950adf2a47fda6df9fa06d2",
-}
-
----@type LazyPluginSpec
-local TelescopeFileBrowser = {
-  url = "https://github.com/nvim-telescope/telescope-file-browser.nvim.git",
-  -- commit 3610dc7 (date unknown, from lazy-lock) https://github.com/nvim-telescope/telescope-file-browser.nvim/commit/3610dc7dc91f06aa98b11dca5cc30dfa98626b7e
-  commit = "3610dc7dc91f06aa98b11dca5cc30dfa98626b7e",
-}
-
----@type LazyPluginSpec
 local Telescope = {
   url = "https://github.com/nvim-telescope/telescope.nvim.git",
-  -- commit a0bbec2 (date unknown, from lazy-lock) https://github.com/nvim-telescope/telescope.nvim/commit/a0bbec21143c7bc5f8bb02e0005fa0b982edc026
-  commit = "a0bbec21143c7bc5f8bb02e0005fa0b982edc026",
+  -- last version check: 2026-03-15
+  -- commit date: 2026-02-16
+  commit = "5255aa27c422de944791318024167ad5d40aad20",
   event = "VimEnter",
   dependencies = {
-    Plenary,
     TelescopeFzfNative,
-    TelescopeUiSelect,
-    TelescopeFileBrowser,
   },
   opts = function()
-    local themes = require("telescope.themes")
     return {
       setup = {
         defaults = {
@@ -63,10 +31,7 @@ local Telescope = {
             },
           },
         },
-        extensions = {
-          ["ui-select"] = themes.get_dropdown(),
-          file_browser = { hidden = true },
-        },
+        extensions = {},
       },
     }
   end,
@@ -75,8 +40,6 @@ local Telescope = {
     telescope.setup(opts.setup)
 
     pcall(telescope.load_extension, "fzf")
-    pcall(telescope.load_extension, "ui-select")
-    pcall(telescope.load_extension, "file_browser")
 
     local builtin = require("telescope.builtin")
     ---@diagnostic disable-next-line: different-requires
@@ -116,11 +79,9 @@ local Telescope = {
     end, { desc = "Search [G]it [F]iles" })
     vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search [G]it [C]ommits" })
     vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Search [G]it [S]tatus" })
-
-    vim.keymap.set("n", "<leader>fb", telescope.extensions.file_browser.file_browser, { desc = "[F]ile [B]rowser" })
   end,
 }
 
-plugins[#plugins + 1] = Telescope
-
-return plugins
+return {
+  Telescope,
+}
